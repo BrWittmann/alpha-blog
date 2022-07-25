506c78617a31840ed895cf8ba77aa08ca88812d8
Brandon <brandon.m.wittmann@gmail.com>
2022-07-25 15:16:42 -0600

    Added login and out buttons. updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,20 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:session][:email].downcase)
-        if user
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "logged in successfully"
+            redirect_to user
+        else
+            flash.now[:alert] = "Either your username or password is incorrect"
+            render 'new'
+        end
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "You have been logged out"
+        redirect_to root_path
     end
 
 end
